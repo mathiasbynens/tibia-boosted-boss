1cd30b81c93d5ca907f7703142beced56d4e2cbd
Mathias Bynens <mathias@qiwi.be>
2023-02-28 16:42:45 +0100

    Harden against delayed website updates
    Issue: https://github.com/mathiasbynens/tibia-boosted-boss/issues/2

--- get-boosted-boss-data.mjs
+++ get-boosted-boss-data.mjs
@@ -12,12 +12,13 @@ const getBossBoostInfo = async () => {
 	const boostableBosses = boostableBossesUgly
 		.map(name => toPrettyName(name))
 		.sort(); // Normalization might affect the sort order.
-	const todaysBoostedBoss = data.boostable_bosses.boosted.name;
-	return {
+	const todaysBoostedBoss = toPrettyName(data.boostable_bosses.boosted.name);
+	const result = {
 		boostableBossesUgly,
 		boostableBosses,
 		todaysBoostedBoss,
-	}
+	};
+	return result;
 };
 
 const {
@@ -40,13 +41,31 @@ await fs.writeFile(
 	stringify(boostableBosses)
 );
 
+const isoDate = (date) => {
+	return date.toISOString().slice(0, 10);
+};
+
+const getDateIds = () => {
+	const date = new Date();
+	const today = isoDate(date);
+	date.setDate(date.getDate() - 1);
+	const yesterday = isoDate(date);
+	return {
+		yesterday,
+		today,
+	};
+};
+
 {
-	const date = new Date().toISOString().slice(0, 'yyyy-mm-dd'.length);
+	const {yesterday, today} = getDateIds();
 	const HISTORY_FILE_PATH = './data/boosted-boss-history.json';
 	const json = await fs.readFile(HISTORY_FILE_PATH, 'utf8');
 	const boostedBossHistory = JSON.parse(json);
-	if (!Object.hasOwn(boostedBossHistory, date)) {
-		boostedBossHistory[date] = toPrettyName(todaysBoostedBoss);
+	if (boostedBossHistory[yesterday] === todaysBoostedBoss) {
+		throw new Error('Upstream website hasn’t updated yet.');
+	}
+	if (!Object.hasOwn(boostedBossHistory, today)) {
+		boostedBossHistory[today] = todaysBoostedBoss;
 	}
 	await fs.writeFile(
 		HISTORY_FILE_PATH,
